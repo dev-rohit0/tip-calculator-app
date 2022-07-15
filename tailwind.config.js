50714b1a7ad7e1d907f346e607d3fb13e5ec7db0
dev-rohit0 <rohit.codein@gmail.com>
2022-07-15 14:56:46 +0530

    some issues fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     screens: {
       'mobile': '375px',
       'desktop': '800px',
-      'sm': '640px',
+      'sm': '810px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px'
